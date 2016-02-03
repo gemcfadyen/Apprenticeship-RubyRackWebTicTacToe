dc849afbf7c239ecadbedef77bb9002d34de7853
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-03 16:19:29 +0000

    Session persistance configured to remember the player option chosen

--- config.ru
+++ config.ru
@@ -2,14 +2,13 @@ lib = File.expand_path("../lib", __FILE__)
 ttt_web = File.expand_path("../../Apprenticeship-RubyWebTicTacToe/lib", __FILE__)
 $:.unshift(lib).unshift(ttt_web)
 
-
 require 'rack_router'
-p "load path: " + $:.to_s
+
 use Rack::Static, :urls => ['public'], :root => '../../Apprenticeship-RubyWebTicTacToe/public'
 use Rack::Static, :urls => ['views'], :root => '../../Apprenticeship-RubyWebTicTacToe/views'
 use Rack::Static, :urls => ['css'], :root => '../../Apprenticeship-RubyWebTicTacToe/public/css'
 use Rack::Session::Cookie,
-  :key => 'my_app_key',
+  :key => 'rack.session',
   :path => '/',
   :expire_after => 14400, # In seconds
   :secret => 'secret_stuff'
--- lib/rack_router.rb
+++ lib/rack_router.rb
@@ -12,8 +12,6 @@ class RackRouter
   def self.call(env)
     request_path = env['REQUEST_PATH']
 
-    p "The request path is: " + request_path.to_s
-
     if request_path == '/'
       template = erb('player_options.erb')
       return  [200, {}, [template]]
@@ -22,10 +20,8 @@ class RackRouter
     if request_path == '/player_options'
       request = Rack::Request.new env
       p "The params are : " + request.params.to_s # contains the union of GET and POST params
-      p "The body is: " + request.body.to_s
-
-      request.session[:player_type] = request.params[:player_type]
-
+      request.session[:player_type] = request.params['player_type']
+      p "Placed choice in session " + request.session.inspect
 
       @game_state = WebTicTacToe.new(
         WebBoardFactory.new(WebDisplayToBoardAdapter.new),
@@ -40,8 +36,7 @@ class RackRouter
 
     if request_path == '/next_move'
       request = Rack::Request.new env
-      p 'session is ' + request.session.to_s
-      p 'earlier player choice taken from session was: ' + request.session[:player_type].to_s
+      p 'earlier player choice taken from session was: ' + request.session[:player_type].inspect
 
       request.params['player_type'] = request.session[:player_type]
 
