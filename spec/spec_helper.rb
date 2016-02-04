540c366122bd1058eb91b3ef985c8ac0d48b68f7
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-04 18:30:27 +0000

    Incrementing the version of core that is used

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,8 +19,7 @@
 
 #Setup Load Path
 lib = File.expand_path("../../lib", __FILE__)
-ttt_lib = File.expand_path("../../../Apprenticeship-RubyTicTacToe/lib", __FILE__)
-$:.unshift(lib).unshift(ttt_lib)
+$:.unshift(lib)
 
 require 'SimpleCov'
 SimpleCov.start
