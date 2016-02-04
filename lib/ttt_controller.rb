f481d0d44594a1a093819ad03236d5a48edd1595
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-04 11:00:33 +0000

    player type is saved in the session

--- lib/ttt_controller.rb
+++ lib/ttt_controller.rb
@@ -1,6 +1,16 @@
 class TTTController
 
   def self.call(env)
-    [200, {}, {}]
+    route = env['REQUEST_PATH']
+    request = Rack::Request.new env
+    if route == '/'
+      [200, {}, {}]
+    elsif route == '/player_options'
+      chosen_player_type = request.params['player_type']
+      chosen_player_type = env['rack.session']['player_type'] = chosen_player_type
+      [200, {}, {}]
+    elsif route == '/next_move'
+      [200, {}, {}]
+    end
   end
 end
--- spec/ttt_controller_spec.rb
+++ spec/ttt_controller_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe TTTController do
   it "can successfully load the page displaying the player options" do
     env = Rack::MockRequest.env_for(
       '/',
-      'REQUEST_PATH' => '/'
+      'REQUEST_PATH' => '/',
+      'REQUEST_METHOD' => 'GET'
     )
 
     status, header, body = TTTController::call(env)
@@ -20,12 +21,16 @@ RSpec.describe TTTController do
     env = Rack::MockRequest.env_for(
       '/',
       'REQUEST_PATH' => '/player_options',
-      'REQUEST_METHOD' => 'POST'
+      'REQUEST_METHOD' => 'POST',
+      :params => {:player_type => '1'},
+      :input => 'player_type=1',
+      'rack.session' => {}
     )
 
     status, header, body = TTTController::call(env)
 
     expect(status).to eq 200
+    expect(env['rack.session']['player_type']).to eq '1'
   end
 
   it "can successfully make a move on the board" do
@@ -34,6 +39,7 @@ RSpec.describe TTTController do
       'REQUEST_PATH' => '/next_move',
       'REQUEST_METHOD' => 'GET',
       'params' => { 'move-taken' => '8', 'grid' => '[0, 1, 2, 3, 4, 5, 6, 7, 8]'},
+      'rack.session' => { "player_type" => "1"},
       :input => "?move-taken=8&grid=[0, 1, 2, 3, 4, 5, 6, 7, 8]"
     )
 
