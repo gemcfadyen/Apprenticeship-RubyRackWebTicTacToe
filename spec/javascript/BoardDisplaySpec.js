8bfabd6da54bd5a9bd144b069435736d35291e4b
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-13 14:29:08 +0000

    BoardDisplay now works with browser and jasmine

--- spec/javascript/BoardDisplaySpec.js
+++ spec/javascript/BoardDisplaySpec.js
@@ -1,11 +1,13 @@
+var BoardDisplay = BoardDisplay|| require('../../lib/javascript/BoardDisplay');
+
 describe("Creates grid containing the correct number of links for display", function() {
-  var BoardDisplay = require('../../lib/javascript/BoardDisplay');
+  //var BoardDisplay = require('../../lib/javascript/BoardDisplay');
 
   it("creates table containing one move and eight links", function() {
 
     var data_for_display =  {"formatted_rows":["X",1,2,3,4,5,6,7,8],"valid_moves":["X","O"],"status":null};
 
-    var updatedTable = new BoardDisplay().paint(data_for_display);
+    var updatedTable = BoardDisplay.paint(data_for_display);
 
     console.log("Table returned from method under test is: " + updatedTable.innerHTML);
     var cells = updatedTable.getElementsByTagName("td");
@@ -19,7 +21,7 @@ describe("Creates grid containing the correct number of links for display", func
 
     var data_for_display =  {"formatted_rows":["X","O","X",3,"X","O","O","X","O"],"valid_moves":["X","O"],"status":null};
 
-    var updatedTable = new BoardDisplay().paint(data_for_display);
+    var updatedTable = BoardDisplay.paint(data_for_display);
     var cells = updatedTable.getElementsByTagName("td");
     var tableLinks = $(cells).find('a');
     var linkText = tableLinks[0].innerHTML;
